552e879b76452e375083ade5a4e15f58e457eb4a
zhengxueyong <897938906@qq.com>
2019-09-11 13:13:49 +0800

    完整整项目911

--- freshshop/freshshop/settings.py
+++ freshshop/freshshop/settings.py
@@ -142,3 +142,39 @@ CACHES = {
       }
   }
 }
+
+LOG_PATH = os.path.join(BASE_DIR, 'logs')
+# 配置日志
+LOGGING = {
+    # 必须是1
+    'version': 1,
+    # 默认为True，禁用日志
+    'disable_existing_loggers': False,
+    # 定义formatters组件，定义存储日志中的格式
+    'formatters':{
+        'default': {
+            'format': '%(levelno)s %(name)s %(asctime)s %(message)s'
+        }
+    },
+    # 定义loggers组件，用于接收日志信息
+    # 并且将日志信息丢给handlers去处理
+    'loggers':{
+        '':{
+            'handlers': ['console'],
+            'level': 'INFO'
+        }
+    },
+    # 定义handlers组件，用户写入日志信息
+    'handlers':{
+        'console':{
+            'level': 'INFO',
+            # 定义存储日志的文件
+            'filename': '%s/log.txt' % LOG_PATH,
+            # 指定写入日志中信息的格式
+            'formatter': 'default',
+            # 指定日志文件超过5M就自动做备份
+            'class': 'logging.handlers.RotatingFileHandler',
+            'maxBytes': 5 * 1024 * 1024,
+        }
+    }
+}
